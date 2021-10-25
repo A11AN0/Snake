3c7f0e6f01d8b4738172f55c2751cbb434687bcb
Allan <matrixboston7613@gmail.com>
2021-10-25 23:23:54 +0800

    made separate file for snake for easier editing

--- snake-game/app.js
+++
@@ -1,9 +0,0 @@
-let lastRenderTime = 0;
-
-const main = (currentTime) =>{
-    window.requestAnimationFrame(main)
-    lastRenderTime = currentTime
-    console.log(currentTime)
-}
-
-window.requestAnimationFrame(main)
