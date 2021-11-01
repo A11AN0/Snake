8ae636efe76672699da89c98dce93eb3f51c4442
Allan <matrixboston7613@gmail.com>
2021-11-02 01:25:20 +0800

    game loop established. Using window.requestAnimationFrame

--- snake/main.js
+++ snake/main.js
@@ -1,16 +1,34 @@
-//1. Since it's probably going to be referenced alot, set a constant name for the arena
-const arena = document.querySelector(".arena")
+const arena = document.querySelector(".arena");
 
-//2. Draw the snake on the board, and set the position using given x/y parameters
-//Probably preferrable to use an object for this..
-const drawSnakeHead = (xCoord, yCoord) =>{
-    const head = document.createElement("div");
-    head.style.gridColumnStart = xCoord
-    head.style.gridRowStart = yCoord
-    head.style.backgroundColor = "green"
-    arena.appendChild(head);   
+
+
+
+
+
+
+let previousTimeStamp = 0
+
+const mainGame = (timeStamp) =>{ 
+    window.requestAnimationFrame(mainGame)
+    const timepassed = (timeStamp - previousTimeStamp)/1000
+    if(timepassed <1/4) return //sets the speed at which screen refreshes/FPS
+    previousTimeStamp = timeStamp
+//-----------------------------------------------------------
+
+//code goes in between here
+
+
+
+
+
+
+
+
+
+
+    
+//------------------------------------------------------------
+    console.log(timepassed)    
 }
 
-drawSnakeHead(5, 7);
-
-//3. A function that will log the arrow keys pressed
+window.requestAnimationFrame(mainGame)
