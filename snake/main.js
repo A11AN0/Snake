0c7acf181e1b910a129528c698aa5ad645c60c4d
Allan <matrixboston7613@gmail.com>
2021-11-02 20:26:51 +0800

    snakeHead travelDirection object, and changeDirection() function implemented

--- snake/main.js
+++ snake/main.js
@@ -1,13 +1,57 @@
+// function which when given an event, (a keydown/keypress), changes the travel direction {} 
+
+const changeDirection = (event)=>{
+    const keyPressed = event.key
+    switch(keyPressed){
+        case "ArrowUp":
+            travelDirection.y = -1
+            travelDirection.x = 0
+            break;
+        case "ArrowDown":
+            travelDirection.y = 1
+            travelDirection.x = 0
+            break;
+        case "ArrowLeft":
+            travelDirection.y = 0
+            travelDirection.x = -1
+            break;
+        case "ArrowRight":
+            travelDirection.y = 0
+            travelDirection.x = 1
+            break;
+    }
+}
+
+
+
+
+
+
 const arena = document.querySelector(".arena");
+const snakeHead = document.querySelector(".snakeHead");
+var headCoordinates = {x: 10, y:10} // Object which sets the starting coordinates for the head of the snake
+var travelDirection = {x:0, y:0} // Object which sets the travel direction for the head of the snake
 
+function drawSnakeHead(){
+    snakeHead.style.backgroundColor = "green"
+    snakeHead.style.gridColumnStart = headCoordinates.x
+    snakeHead.style.gridRowStart = headCoordinates.y
+}
+//This is a function which draws the head of my snake
 
-
+function updateSnakeHead(){
+    headCoordinates.x += travelDirection.x;
+    headCoordinates.y += travelDirection.y;
+}
+/*This function updates the coordinates of the snakeHead {headCoordinates}, based on the travel directions, given
+by the function changeDirection()*/
 
 
 
 
 let previousTimeStamp = 0
 
+//This one was interesting to read about, this is the game loop I made using window.requestAnimationFrame
 const mainGame = (timeStamp) =>{ 
     window.requestAnimationFrame(mainGame)
     const timepassed = (timeStamp - previousTimeStamp)/1000
@@ -16,6 +60,16 @@ const mainGame = (timeStamp) =>{
 //-----------------------------------------------------------
 
 //code goes in between here
+
+updateSnakeHead();//at least we know it works lol
+drawSnakeHead();
+document.addEventListener("keydown", changeDirection)
+
+
+
+
+
+
 
 
 
@@ -28,7 +82,7 @@ const mainGame = (timeStamp) =>{
 
     
 //------------------------------------------------------------
-    console.log(timepassed)    
+    console.log(timepassed)//simply logs timepassed/ was a test that the game loop works
 }
 
 window.requestAnimationFrame(mainGame)
