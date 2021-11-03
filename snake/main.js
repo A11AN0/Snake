4391339b64bc8dd4761072f806209239bee0e7f3
Allan <matrixboston7613@gmail.com>
2021-11-04 02:30:18 +0800

    changed global variables to constants

--- snake/main.js
+++ snake/main.js
@@ -31,34 +31,34 @@ const changeDirection = (event)=>{
 
 const arena = document.querySelector(".arena");
 const snakeHead = document.querySelector(".snakeHead");
-const snakeGrowth = 4
-var headCoordinates = {x: 10, y:10} // Object which sets the starting coordinates for the head of the snake
-var travelDirection = {x:0, y:0} // Object which sets the travel direction for the head of the snake
-var snakeBody = [] // Array for the body of the snake
-var snakeLength = 0 // Array for the length of the snake
+const snakeGrowth = 7
+const headCoordinates = {x: 10, y:10} // Object which sets the starting coordinates for the head of the snake
+const travelDirection = {x:0, y:0} // Object which sets the travel direction for the head of the snake
+const snakeBody = [] // Array for the body of the snake
 
 
 
+//This is a function which draws the head of my snake // although since it's already in arena div, might come back and remove this function (probs not tho)
 function drawSnakeHead(){
     snakeHead.style.backgroundColor = "green"
     snakeHead.style.gridColumnStart = headCoordinates.x
     snakeHead.style.gridRowStart = headCoordinates.y
 }
-//This is a function which draws the head of my snake
 
+
+/*This function updates the coordinates of the snakeHead {headCoordinates}, based on the travel directions, given
+by the function changeDirection()*/
 function updateSnakeHead(){
     headCoordinates.x += travelDirection.x;
     headCoordinates.y += travelDirection.y;
     snakeBody[0] = snakeHead
 }
-/*This function updates the coordinates of the snakeHead {headCoordinates}, based on the travel directions, given
-by the function changeDirection()*/
 
 
 
 //This is the function which will draw the body of my snake
 function drawSnakeBody(){
-    if(snakeBody.length<6){ // made it so it only draws snake body depending on the allowed length
+    if(snakeBody.length<snakeGrowth){ // sets number of segments on snake (number of segments = value)
     const section = document.createElement("div");
     section.style.backgroundColor = "red"
     section.style.gridColumnStart = headCoordinates.x 
@@ -66,16 +66,14 @@ function drawSnakeBody(){
     arena.appendChild(section);
     snakeBody.push(section);
     }
-} //wont stop proliferating in game loop
+} 
 
 //This is the function which will update the body of the snake
 function updateSnakeBody(){
-    
-        for(var i=snakeBody.length-1; i>= 1; i--){
-            snakeBody[i].style.gridColumnStart = snakeBody[i-1].style.gridColumnStart
-            snakeBody[i].style.gridRowStart = snakeBody[i-1].style.gridRowStart // at least we have 1 lol
-        }
-    
+    for(var i=snakeBody.length-1; i>= 1; i--){ //i=0, is like a type of train  carriage hence why i>=1
+        snakeBody[i].style.gridColumnStart = snakeBody[i-1].style.gridColumnStart
+        snakeBody[i].style.gridRowStart = snakeBody[i-1].style.gridRowStart // at least we have 1 lol
+    }    
 }
 
 //This is the function which will draw the food on the board
@@ -103,22 +101,26 @@ function keepScore(){
 
 }
 
+
+
+
 let previousTimeStamp = 0
 
-//This one was interesting to read about, this is the game loop I made using window.requestAnimationFrame
+//This one was interesting to read online about, this is the game loop I made using window.requestAnimationFrame
 const mainGame = (timeStamp) =>{ 
     window.requestAnimationFrame(mainGame)
     const timepassed = (timeStamp - previousTimeStamp)/1000
     if(timepassed <1/4) return //sets the speed at which screen refreshes/FPS
     previousTimeStamp = timeStamp
-//-----------------------------------------------------------
 
-//code goes in between here
+//---------------------//Code for item movemement goes in between here//--------------------------------------
+
 
 updateSnakeHead();//at least we know it works lol
 drawSnakeHead();
+updateSnakeBody();//slightly more responsive/quicker like this
 drawSnakeBody(); //will need to make function so that if food is found, snakebody[] length increases
-updateSnakeBody();
+
 document.addEventListener("keydown", changeDirection)
 
 
@@ -129,6 +131,4 @@ document.addEventListener("keydown", changeDirection)
 
 window.requestAnimationFrame(mainGame)
 
-
-//Time to make a function which adds segments to the snake
 
